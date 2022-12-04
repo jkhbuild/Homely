da41b8f3cef7fd2f73e7c7f6357ec62e789932af
jkhbuild <jkh.build@gmail.com>
2022-12-04 14:04:41 -0500

    redux store setup tested

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
+import configureStore from "./store";
+
+// remove later
+const store = configureStore();
+
+if (process.env.NODE_ENV !== "production") {
+  window.store = store;
+}
+// remove later
+
+function Root() {
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  );
+}
 
 const initializeApp = () => {
   ReactDOM.render(
