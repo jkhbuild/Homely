184fa990e4d12e3af855f547ff697ce8350a3aa2
jkhbuild <jkh.build@gmail.com>
2022-12-03 20:36:42 -0500

    user migration table and db created

---
+++ db/migrate/20221203212507_create_users.rb
@@ -0,0 +1,16 @@
+class CreateUsers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :users do |t|
+      t.string :username, null: false
+      t.string :password_digest, null: false
+      t.string :session_token, null: false
+      t.boolean :is_professional, null: false
+      t.string :role
+
+      t.timestamps
+    end
+    add_index :users, :email, unique: true
+    add_index :users, :username, unique: true
+    add_index :users, :session_token, unique: true
+  end
+end
