95fbdf669ae5c5d67206663fe06677a5f4d86800
jkhbuild <jkh.build@gmail.com>
2022-12-04 13:08:08 -0500

    imports added to store/index.js

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,1 +1,2 @@
-import {createStore, coombineReducers, applyMiddleware, compse } from redux
+import { createStore, coombineReducers, applyMiddleware, compse } from "redux";
+import thunk from "redux-thunk";
