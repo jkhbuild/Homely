501855827d75a7c3a292f894fcc963d33b57b992
jkhbuild <jkh.build@gmail.com>
2022-12-04 22:16:31 -0500

    navigation added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,19 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import LoginFormPage from "./components/LoginFormPage/index.js";
-// import Navigation from "./components/Navigation";
+import SignUpFormPage from "./components/SignUpFormPage/index.js";
+import Navigation from "./components/Navigation";
 
 function App() {
   return (
     <>
+      <Navigation />
       <Switch>
         <Route path="/login">
           <LoginFormPage />
+        </Route>
+        <Route path="/signup">
+          <SignUpFormPage />
         </Route>
       </Switch>
     </>
