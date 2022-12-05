98c1f624ffb6ad21d0623fee95c4e5b1f90aeed5
jkhbuild <jkh.build@gmail.com>
2022-12-05 12:10:51 -0500

    modal for signup created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,11 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
-import LoginForm from "./components/LoginFormModal/index.js";
-import SignUpFormPage from "./components/SignUpFormPage/index.js";
+// import { Route, Switch } from "react-router-dom";
 import Navigation from "./components/Navigation";
 
 function App() {
   return (
     <>
       <Navigation />
-      <Switch>
-        <Route path="/login">
-          <LoginForm />
-        </Route>
-        <Route path="/signup">
-          <SignUpFormPage />
-        </Route>
-      </Switch>
     </>
   );
 }
--- frontend/src/components/SignUpFormModal/SignUpForm.js
+++ frontend/src/components/SignUpFormModal/SignUpForm.js
@@ -1,10 +1,10 @@
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Redirect } from "react-router-dom";
+// import { Redirect } from "react-router-dom";
 import * as sessionActions from "../../store/session";
 import "./SignUpForm.css";
 
-function SignupFormPage() {
+function SignUpForm() {
   const dispatch = useDispatch();
   const sessionUser = useSelector((state) => state.session.user);
   const [email, setEmail] = useState("");
@@ -16,7 +16,7 @@ function SignupFormPage() {
   const [confirmPassword, setConfirmPassword] = useState("");
   const [errors, setErrors] = useState([]);
 
-  if (sessionUser) return <Redirect to="/" />;
+  // if (sessionUser) return <Redirect to="/" />;
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -50,16 +50,43 @@ function SignupFormPage() {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form id="signup" onSubmit={handleSubmit}>
       <ul>
         {errors.map((error) => (
           <li key={error}>{error}</li>
         ))}
       </ul>
-      <h1>Create an Account</h1>
 
-      <label>
-        Email
+      <div class="modal-header">
+        <h1>Create an Account</h1>
+        <button class="modal-close-button">
+          <i class="fa-solid fa-circle-info fa-2xl"></i>
+        </button>
+      </div>
+
+      <div id="users-name">
+        <label class="user-input">
+          First Name
+          <input
+            type="text"
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
+            required
+          />
+        </label>
+        <label class="user-input">
+          Last Name
+          <input
+            type="text"
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
+            required
+          />
+        </label>
+      </div>
+
+      <label class="user-input">
+        Email Address
         <input
           type="text"
           value={email}
@@ -67,7 +94,16 @@ function SignupFormPage() {
           required
         />
       </label>
+
       <label>
+        I am an industry professional
+        <input
+          type="checkbox"
+          checked={isProfessional}
+          onChange={(e) => setIsProfessional(e.target.checked)}
+        />
+      </label>
+      <label class="user-input">
         Password
         <input
           type="password"
@@ -76,7 +112,7 @@ function SignupFormPage() {
           required
         />
       </label>
-      <label>
+      <label class="user-input">
         Confirm Password
         <input
           type="password"
@@ -85,35 +121,10 @@ function SignupFormPage() {
           required
         />
       </label>
-      <label>
-        First Name
-        <input
-          type="text"
-          value={firstName}
-          onChange={(e) => setFirstName(e.target.value)}
-          required
-        />
-      </label>
-      <label>
-        Last Name
-        <input
-          type="text"
-          value={lastName}
-          onChange={(e) => setLastName(e.target.value)}
-          required
-        />
-      </label>
-      <label>
-        I am an industry professional
-        <input
-          type="checkbox"
-          value={isProfessional}
-          onChange={(e) => setIsProfessional(!isProfessional)}
-        />
-      </label>
+
       <button type="submit">Sign Up</button>
     </form>
   );
 }
 
-export default SignupFormPage;
+export default SignUpForm;
