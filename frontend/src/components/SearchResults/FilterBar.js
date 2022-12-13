ba12bba59fdd66659fd1a11a382924d97880de85
jkhbuild <jkh.build@gmail.com>
2022-12-13 16:41:58 -0500

    filter bar placeholder added

--- frontend/src/components/SearchResults/FilterBar.js
+++ frontend/src/components/SearchResults/FilterBar.js
@@ -3,39 +3,50 @@ import { useHistory } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 function FilterBar() {
-  <div className="filter-container">
-    <input type="text" placeholder="Location or Point of Interest"></input>
-    <select className="beds-type-filter">
-      <option>studio</option>
-      <option>1.0</option>
-      <option>2.0</option>
-      <option>3.0</option>
-      <option>4.0</option>
-      <option>5.0</option>
-      <option>6.0</option>
-    </select>
-    <select className="baths-type-filter">
-      <option>0.5</option>
-      <option>1.0</option>
-      <option>1.5</option>
-      <option>2.0</option>
-      <option>2.5</option>
-      <option>3.0</option>
-      <option>4.0</option>
-      <option>4.5</option>
-      <option>5.0</option>
-      <option>5.5</option>
-      <option>6.0</option>
-      <option>6.5</option>
-    </select>
-    <select className="property-type-filter">
-      <option>Apartment</option>
-      <option>Single Family House</option>
-      <option>Condominum</option>
-      <option>Townhouse</option>
-      <option>Mobile Home/Manufactured Home</option>
-    </select>
-  </div>;
+  return (
+    <div className="filter-container">
+      <input type="text" placeholder="Location or Point of Interest"></input>
+      <select className="beds-type-filter">
+        <option value="" selected disabled hidden>
+          Beds
+        </option>
+        <option>studio</option>
+        <option>1.0</option>
+        <option>2.0</option>
+        <option>3.0</option>
+        <option>4.0</option>
+        <option>5.0</option>
+        <option>6.0</option>
+      </select>
+      <select className="baths-type-filter">
+        <option value="" selected disabled hidden>
+          Baths
+        </option>
+        <option>0.5</option>
+        <option>1.0</option>
+        <option>1.5</option>
+        <option>2.0</option>
+        <option>2.5</option>
+        <option>3.0</option>
+        <option>4.0</option>
+        <option>4.5</option>
+        <option>5.0</option>
+        <option>5.5</option>
+        <option>6.0</option>
+        <option>6.5</option>
+      </select>
+      <select className="property-type-filter">
+        <option value="" selected disabled hidden>
+          Type
+        </option>
+        <option>Apartment</option>
+        <option>Single Family House</option>
+        <option>Condominum</option>
+        <option>Townhouse</option>
+        <option>Mobile Home/Manufactured Home</option>
+      </select>
+    </div>
+  );
 }
 
 export default FilterBar;
--- frontend/src/components/SearchResults/index.js
+++ frontend/src/components/SearchResults/index.js
@@ -3,13 +3,22 @@ import { useHistory } from "react-router-dom";
 import { useSelector } from "react-redux";
 import FilterBar from "./FilterBar";
 import Map from "./Map";
+import "./SearchResults.css";
 
 function SearchResults() {
-  <div className="filter-container">
-    <FilterBar />
-    hello
-  </div>;
-  <div className="listing-cards-container"></div>;
+  return (
+    <>
+      <div className="filter-container">
+        <FilterBar />
+      </div>
+      <div className="search-results-main-content">
+        <div className="map-container">
+          <Map />
+        </div>
+        <div className="listing-cards-container"></div>
+      </div>
+    </>
+  );
 }
 
 export default SearchResults;
