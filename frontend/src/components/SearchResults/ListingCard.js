007f68db5dfef573e8b3ed4d758aa88b7e0f2a78
jkhbuild <jkh.build@gmail.com>
2022-12-14 12:44:37 -0500

    90% css for listing cards

--- frontend/src/components/SearchResults/ListingCard.js
+++ frontend/src/components/SearchResults/ListingCard.js
@@ -12,25 +12,35 @@ function ListingCard({ listing }) {
   return (
     <article className="listing-card">
       <div className="listing-card-header">
-        <div>
-          <h2 className="listing-card-address">{listing.address}</h2>
-          <p className="listing-card-csz">
-            {listing.city}, {listing.state} {listing.zip}
-          </p>
+        <div className="listing-card-header-left">
+          <div className="listing-card-header-left-subcontainer">
+            <h2 className="listing-card-address">{listing.address}</h2>
+            <h4 className="listing-card-csz">
+              {listing.city}, {listing.state} {listing.zip}
+            </h4>
+          </div>
         </div>
-        <div>
-          <button>
-            <i class="fa-regular fa-heart"></i>
+        <div className="listing-card-header-right">
+          <button className="listing-card-favorite">
+            <i className="fa-regular fa-heart"></i>
           </button>
         </div>
       </div>
-      <div>
-        <img></img>
-        <p>${listing.rent}</p>
-        <p>{listing.beds} Beds</p>
-        <p>{listing.propertyType} for Rent</p>
-
-        <button>Email</button>
+      <div className="listing-card-body">
+        <div className="listing-card-body-left">
+          <img
+            src={require("../../assets/listing-card-placeholder.jpg")}
+            alt=""
+          ></img>
+        </div>
+        <div className="listing-card-body-right">
+          <h3 className="listing-card-rent">${listing.rent} </h3>
+          <h3 className="listing-card-bb">{listing.beds} Beds</h3>
+          <h4 className="listing-card-pt"> {listing.propertyType} for Rent </h4>
+          {/* <p>{listing.beds} Beds</p>
+          <p>{listing.propertyType} for Rent</p> */}
+          <button className="listing-card-button">Email</button>
+        </div>
       </div>
     </article>
   );
