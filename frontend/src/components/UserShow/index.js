d9c5351a2206158cae68ec0a980dbb8dc5889d78
jkhbuild <jkh.build@gmail.com>
2023-01-10 18:58:34 -0500

    import css for UserShow

--- frontend/src/components/UserShow/index.js
+++ frontend/src/components/UserShow/index.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useHistory } from "react-router-dom";
+import "./UserShow.css";
 
 function UserShow() {
   return (
