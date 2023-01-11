459ce83fae6a8aca657fee1b6bf4e2305832dcea
jkhbuild <jkh.build@gmail.com>
2023-01-10 23:03:28 -0500

    set up user show listing cards component

---
+++ frontend/src/components/UserShow/UserShowListingCards.js
@@ -0,0 +1,10 @@
+import React, { useEffect, useState, useMemo } from "react";
+import { useHistory, useParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import * as propertyActions from "../../store/listings";
+
+function UserShowListingCards() {
+  const history = useHistory();
+}
+
+export default UserShowListingCards;
