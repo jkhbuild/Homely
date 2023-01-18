43fde26aed289bba54c41d8a5bd617b4d0465413
jkhbuild <jkh.build@gmail.com>
2023-01-17 22:35:14 -0500

    migrate likes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_08_211946) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_18_031211) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -40,6 +40,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_08_211946) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "listing_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["listing_id"], name: "index_likes_on_listing_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
   create_table "listings", force: :cascade do |t|
@@ -86,5 +95,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_08_211946) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "likes", "listings"
+  add_foreign_key "likes", "users"
   add_foreign_key "listings", "users", column: "owner_id"
 end
