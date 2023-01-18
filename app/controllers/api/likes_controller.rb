2f6d9ef325e00b2515d88d0b7b9f70f6f07d4b2f
jkhbuild <jkh.build@gmail.com>
2023-01-18 15:37:13 -0500

    edit likes controller

--- app/controllers/api/likes_controller.rb
+++ app/controllers/api/likes_controller.rb
@@ -2,13 +2,21 @@ class LikesController < ApplicationController
     wrap_parameters include: Like.attribute_names + ['userId', 'listingId']
     
     def create
-        like = Like.new(like_params)
-        if like.save
-            @like = Like.where(user_id: like.user_id)
-            render :index
+
+        @like = current_user.likes.new(like_params)
+
+        if @like.save
+            render index
         else
-            render json { error: 'test error'}
+            render json {error: 'like could not be created'}
         end
+        # like = Like.new(like_params)
+        # if like.save
+        #     @like = Like.where(user_id: like.user_id)
+        #     render :index
+        # else
+        #     render json { error: 'test error'}
+        # end
     end
 
     def index
@@ -17,8 +25,10 @@ class LikesController < ApplicationController
     end
 
     def destroy
-        like = Like.find_by(id: params[:id])
-        like.delete
+        @like = current_user.likes.find(params[:id]
+        @like.destroy
+        # like = Like.find_by(id: params[:id])
+        # like.delete
     end
 
     private
