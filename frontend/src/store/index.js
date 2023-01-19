ff09bbf25bc9fe329c5a30a62f7da33f202b7077
jkhbuild <jkh.build@gmail.com>
2023-01-18 21:30:25 -0500

    add likes to rootreducer

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -2,10 +2,12 @@ import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import session from "./session";
 import listings from "./listings";
+import likes from "./likes";
 
 export const rootReducer = combineReducers({
   session,
   listings,
+  likes,
 });
 
 let enhancer;
