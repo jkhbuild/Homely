ed90eb1bd16acdabe4ef2ced7a83a1f1ee74bb3d
jkhbuild <jkh.build@gmail.com>
2023-02-02 22:18:38 -0500

    remove unused imports from SearchBar

--- frontend/src/components/Splash/SearchBar.js
+++ frontend/src/components/Splash/SearchBar.js
@@ -1,7 +1,6 @@
-import React, { useState, useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
 import { useHistory } from "react-router-dom";
-import * as propertyActions from "../../store/listings";
 import "./Splash.css";
 
 function SearchBar() {
